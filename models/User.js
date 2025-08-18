945b59059a1ed6f568d166da27b2d089230f6fd9
Shreya Savni <it.20213061@gmail.com>
2025-08-19 01:42:11 +0530

    chat api

--- models/User.js
+++ models/User.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  user_name: String,
-  email: { type: String, unique: true },
-  password: String,
+  user_name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  phone: { type: String },
+  description: { type: String },
+  amount: { type: Number, default: 0 },
+  discount: { type: Number, default: 0 },
+  tax: { type: Number, default: 0 },
+  totalAmount: { type: Number, default: 0 },
   otp: String,
   otpExpires: Date,
   verified: { type: Boolean, default: false },
+
+  // NEW
+  role: { type: String, enum: ["user", "admin"], default: "user" }
 });
+
 
 export default mongoose.model("User", userSchema);
