5e2a97cda2a2a598623050e82fa86e2c6131e565
Shreya Savni <it.20213061@gmail.com>
2025-08-19 09:29:46 +0530

    changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import quotationRoutes from "./routes/quotationRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -18,8 +19,8 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 app.use("/api/booking", bookingRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/quotation", quotationRoutes);
+app.use("/api/chat", chatRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
