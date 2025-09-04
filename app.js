2a985ad2be951161217208434247a8325e322248
Shreya Savni <it20213061@gmail.com>
2025-09-04 17:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ import path from "path";
 
 import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import bookingRoutes from "./routes/bookingRoutes.js";
-import quotationRoutes from "./routes/quotationRoutes.js";
-import chatRoutes from "./routes/chatRoutes.js";
-import naturePaymentRoutes from "./routes/NaturePaymentRoutes.js";
-import tdsRoutes from './routes/tdsRoutes.js';
+
 
 
 // Import Routes
